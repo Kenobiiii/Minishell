89691892625660858f59e624e1bf6b5b6aa9b6c0
Pablo Pons <pablo.romeroponsfuster@gmail.com>
2024-11-07 17:50:07 +0100

    Creadas structs y arreglada norminette
    e_type
    	- define el tipo que es la variable
    
    s_cmd
    	- guarda comandos simples
    
    s_redirect
    	- guarda las redirecciones
    
    s_ast
    	- guarda el tipo
    	- guarda comandos/ficheros que hay a ambos lados
    	- llama a cmd o redirect
    
    s_tokens
    	- guarda cada parte de la linea dada en tokens
    
    s_env
    	- guarda el enviroment
    
    arreglos norminette

--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 19:06:45 by paromero          #+#    #+#             */
-/*   Updated: 2024/11/05 19:37:37 by paromero         ###   ########.fr       */
+/*   Updated: 2024/11/07 17:49:57 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,30 +28,74 @@
 # include <curses.h>
 # include <ncurses.h>
 
-# define WRONG_INPUT 1
-# define MALLOC_ERROR 2
+typedef enum e_type
+{
+	CMD,
+	REDIRECT,
+	PIPE,
+	AND,
+	OR
+}	t_type;
+//- struct para guardar los comandos simples
+typedef struct s_cmd
+{
+	char	**args;
+}	t_cmd;
+
+//- struct para manejar la redirección
+typedef struct s_redirect
+{
+	t_type			type; //- guarda el tipo de redirección que es
+	struct s_ast	*left; //- guarda el comando de la izquierda
+	char			*file; //- guarda el archivo al que se va a añadir
+}	t_redirect;
+
+typedef struct s_ast
+{
+	t_type			type;
+	struct s_ast	*left;
+	struct s_ast	*right;
+	union
+	{
+		t_cmd		cmd;
+		t_redirect	redirect;
+	} u_uniond;
+}	t_ast;
+
+/**
+ *- Uniones:
+ *- son variables que comparten la misma memoria para así ahorrarla
+ *- si una se modifica la otra también, útil para casos donde depende
+ *- del tipo se necesita una cosa u otra
+ */
+
+typedef struct s_tokens
+{
+	t_type		type;
+	char		*value;
+	t_tokens	*next;
+}	t_tokens;
 
 typedef struct s_env
 {
-	char  	*value;
-	struct s_env *next;
+	char			*value;
+	struct s_env	*next;
 }	t_env;
 
 typedef struct s_data
 {
-	char	*line //- linea comando actual
-	char	*prompt //- Promp de salida de la minishell (ej: minishell> )
-	char	*cwd //- directorio actual
-	int		last_exit_status //- salida ultimo comando ejecutado ($?)
-	int		signal_received //- manejo de las señales
-	pid_t	pid //- Proceso hijo y padre
-	t_env	*env //- Puntero a estructuar de env
-	t_token	*tokens //- puntero a estructura de tokens
-	t_ast	*ast //- puntero a estructura ast (Abstract Syntax Tree)	
+	char		*line; //- linea comando actual
+	char		*prompt; //- Promp de salida de la minishell (ej: minishell> )
+	char		*cwd; //- directorio actual
+	int			last_exit_status; //- salida ultimo comando ejecutado ($?)
+	int			signal_received; //- manejo de las señales
+	pid_t		pid; //- Proceso hijo y padre
+	t_env		*env; //- Puntero a estructuar de env
+	t_tokens	*tokens; //- puntero a estructura de tokens
+	t_ast		*ast; //- puntero a estructura ast (Abstract Syntax Tree)	
 }	t_data;
 
 #endif
-
 /**
  * - explicación
  * * Importante
