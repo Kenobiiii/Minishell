7931aa9c6adbcde22ff28831cd207c7031bc9a0a
Pablo Pons <pablo.romeroponsfuster@gmail.com>
2024-11-07 19:46:56 +0100

    Empezado main, verifica si no está vacío y pasa env

---
+++ src/main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 17:54:48 by paromero          #+#    #+#             */
+/*   Updated: 2024/11/07 17:58:52 by paromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int main(int ac, char **av, char **env)
+{
+    t_data data;
+    char *read;
+    
+    if (ac != 1  || env == NULL || *env == NULL)
+    {
+        printf("Error: No environment found. Exiting...");
+        return (0);
+    }
+    (void)ac;
+    (void)av;
+    
+}
