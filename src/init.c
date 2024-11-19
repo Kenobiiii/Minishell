ce8f994c239bebe123396a9cc467e0fdc8f3d4de
Pablo Pons <pablo.romeroponsfuster@gmail.com>
2024-11-19 17:38:32 +0100

    creada func para guardar el env

--- src/init.c
+++ src/init.c
@@ -6,12 +6,55 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 17:53:02 by paromero          #+#    #+#             */
-/*   Updated: 2024/11/07 19:15:41 by paromero         ###   ########.fr       */
+/*   Updated: 2024/11/19 17:30:58 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+t_env	*create_node(const char *valor)
+{
+	t_env	*new_node;
+
+	new_node = (t_env *)malloc(sizeof(t_env));
+	if (!new_node)
+		return (NULL);
+	new_node->value = ft_strdup(valor);
+	if (!new_node->value)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = (NULL);
+	return (new_node);
+}
+
+int	init_env(t_data *data, char *env[])
+{
+	t_env	*current;
+	int		i;
+
+	if (env == NULL || env[0] == NULL)
+	{
+		printf("El entorno está vacío.\n");
+		data->env_head = NULL;
+		return (1);
+	}
+	data->env_head = create_node(env[0]);
+	if (!data->env_head)
+		return (0);
+	current = data->env_head;
+	i = 1;
+	while (env[i])
+	{
+		current->next = create_node(env[i]);
+		if (!current->next)
+			return (0);
+		current = current->next;
+		i++;
+	}
+	return (1);
+}
 
 int	init_data(t_data *data, char **env)
 {
