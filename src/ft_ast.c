c89f37f0e59bf89619fdab706f06179fe9ac6247
paromero <pablo.romeroponsfuster@gmail.com>
2025-01-09 19:59:24 +0100

    cambios en ast

--- src/ft_ast.c
+++ src/ft_ast.c
@@ -6,101 +6,78 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 17:26:30 by paromero          #+#    #+#             */
-/*   Updated: 2024/12/17 18:41:06 by paromero         ###   ########.fr       */
+/*   Updated: 2025/01/09 19:39:53 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_ast	*ft_new_node(t_type type, char *value)
+t_ast	*ft_create_ast_node(t_type type, char *value)
 {
-	t_ast	*new_node;
-
-	new_node = malloc(sizeof(t_ast));
-	if (!new_node)
+	t_ast	*node = malloc(sizeof(t_ast));
+	if (!node)
 		return (NULL);
-	new_node->type = type;
-	new_node->value = ft_strdup(value);
-	if (!new_node->value)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->args = NULL;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	return (new_node);
+	node->type = type;
+	node->value = ft_strdup(value);
+	node->args = NULL;
+	node->right = NULL;
+	node->left = NULL;
+	return (node);
 }
 
-t_ast	*ft_ast(t_tokens *tokens)
+t_ast	*ft_build_ast(t_tokens *tokens)
 {
-	t_ast		*root;
-	t_ast		*current;
-	t_ast		*new_node;
-	t_tokens	*tmp;
-	int			i;
-	int			arg_count;
+	t_ast	*root;
+	t_ast	*current_cmd;
+	t_ast	*new_node;
 
 	root = NULL;
-	current = NULL;
+	current_cmd = NULL;
 	while (tokens)
 	{
 		if (tokens->type == CMD)
 		{
-			new_node = ft_new_node(CMD, tokens->value);
-			tmp = tokens->next;
-			arg_count = 0;
-			while (tmp && tmp->type == CMD)
-			{
-				arg_count++;
-				tmp = tmp->next;
-			}
-			if (arg_count > 0)
-			{
-				new_node->args = malloc(sizeof(char *) * (arg_count + 2));
-				new_node->args[0] = ft_strdup(tokens->value);
-				i = 1;
-				tokens = tokens->next;
-				while (tokens && tokens->type == CMD)
-				{
-					new_node->args[i++] = ft_strdup(tokens->value);
-					tokens = tokens->next;
-				}
-				new_node->args[i] = NULL;
-			}
+			// Crear un nuevo nodo para el comando
+			new_node = ft_create_ast_node(tokens->value, CMD);
+			// Si hay un comando actual, agregar el argumento
+			if (current_cmd)
+				ft_add_argument(current_cmd, tokens->value);
 			else
-				tokens = tokens->next;
-			if (!root)
-				root = new_node;
-			else if (current && current->type == PIPE)
-				current->right = new_node;
-			else
-				current->left = new_node;
-			current = new_node;
-		}
-		else if (tokens->type == PIPE)
-		{
-			new_node = ft_new_node(PIPE, tokens->value);
-			new_node->left = root;
-			root = new_node;
-			current = new_node;
-			tokens = tokens->next;
-		}
-		else if (tokens->type == REDIRECT_OUT || tokens->type == REDIRECT_IN
-			|| tokens->type == REDIN2 || tokens->type == REDOUT2)
-		{
-			new_node = ft_new_node(tokens->type, tokens->value);
-			tokens = tokens->next;
-			if (tokens && tokens->type == CMD)
 			{
-				new_node->right = ft_new_node(CMD, tokens->value);
-				tokens = tokens->next;
+				// Primer comando, establecer como nodo actual
+				current_cmd = new_node;
+				if (!root)
+					root = current_cmd;
 			}
-			if (current)
-				current->right = new_node;
 		}
 		else
-			tokens = tokens->next;
+		{
+			// Crear nodos para operadores y conectar
+			new_node = ft_create_ast_node(tokens->value, tokens->type);
+			new_node->left = root;  // El operador conecta el árbol existente
+			root = new_node;
+			current_cmd = NULL; // Resetear para el siguiente comando
+		}
+		tokens = tokens->next;
 	}
-	return (root);
+	return root;
+}
+
+void	ft_add_argument(t_ast *cmd_node, char *arg)
+{
+	int		i;
+	char	**new_args;
+
+	i = 0;
+	while (cmd_node->args && cmd_node->args[i])
+		i++;
+	new_args = malloc(sizeof(char *) * (i + 2));
+	if (!new_args)
+		return; // Manejar error de memoria si es necesario
+	while (i--)
+		new_args[i] = cmd_node->args[i];
+	new_args[i + 1] = ft_strdup(arg);
+	new_args[i + 2] = NULL;
+	free(cmd_node->args);
+	cmd_node->args = new_args;
 }
