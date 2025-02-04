61b471093a0b43e23011db38f39e802775ff2378
paromero <pablo.romeroponsfuster@gmail.com>
2025-02-04 13:16:08 +0100

    arreglada norm en handle_quotes

--- src/parse/ft_handle_quotes.c
+++ src/parse/ft_handle_quotes.c
@@ -3,25 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_handle_quotes.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anggalle <anggalle@student.42.fr>          +#+  +:+       +#+        */
+/*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 12:33:32 by paromero          #+#    #+#             */
-/*   Updated: 2025/02/04 12:53:40 by anggalle         ###   ########.fr       */
+/*   Updated: 2025/02/04 13:15:42 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-char	*ft_handle_single(char	*line)
+int	ft_quotelen(char	*line)
 {
-	char	*new_line;
-	int		i;
-	int		j;
-	int		count;
+	int	i;
+	int	count;
 
-	count = 0;
-	j = 0;
 	i = 0;
+	count = 0;
 	while (line[i])
 	{
 		if (line[i] == '\'')
@@ -30,10 +27,22 @@ char	*ft_handle_single(char	*line)
 			i++;
 		count++;
 	}
+	return (count);
+}
+
+char	*ft_handle_single(char	*line)
+{
+	char	*new_line;
+	int		i;
+	int		j;
+	int		count;
+
+	j = 0;
+	i = 0;
+	count = ft_quotelen(line);
 	new_line = (char *)malloc(sizeof(char) * (count + 1));
 	if (!new_line)
 		return (0);
-	i = 0;
 	while (line[i])
 	{
 		if (line[i] == '\'')
@@ -56,18 +65,10 @@ char	*ft_handle_double(char	*line)
 	count = 0;
 	j = 0;
 	i = 0;
-	while (line[i])
-	{
-		if (line[i] == '"')
-			i++;
-		if (line[i])
-			i++;
-		count++;
-	}
+	count = ft_quotelen(line);
 	new_line = (char *)malloc(sizeof(char) * (count + 1));
 	if (!new_line)
 		return (0);
-	i = 0;
 	while (line[i])
 	{
 		if (line[i] == '"')
@@ -78,6 +79,28 @@ char	*ft_handle_double(char	*line)
 	}
 	new_line[j] = '\0';
 	return (new_line);
+}
+
+int	handle_quotes(t_data *data, char **matrix, int count_x, int count_y)
+{
+	char	*new_line;
+
+	if (matrix[count_x][count_y] == '\'')
+	{
+		new_line = ft_handle_single(matrix[count_x]);
+		free(matrix[count_x]);
+		matrix[count_x] = new_line;
+		return (1);
+	}
+	else if (matrix[count_x][count_y] == '"')
+	{
+		new_line = ft_handle_double(matrix[count_x]);
+		new_line = ft_handledollar(data, new_line);
+		free(matrix[count_x]);
+		matrix[count_x] = new_line;
+		return (1);
+	}
+	return (0);
 }
 
 void	ft_handle_quotes(t_data	*data, char	**matrix)
@@ -92,25 +115,9 @@ void	ft_handle_quotes(t_data	*data, char	**matrix)
 	{
 		quote_found = 0;
 		count_y = 0;
-		while(matrix[count_x][count_y])
+		while (matrix[count_x][count_y] && quote_found != 1)
 		{
-			if (matrix[count_x][count_y] == '\'')
-			{
-				new_line = ft_handle_single(matrix[count_x]);
-				free (matrix[count_x]);
-				matrix[count_x] = new_line;
-				quote_found = 1;
-				break ;
-			}
-			else if (matrix[count_x][count_y] == '"')
-			{
-				new_line = ft_handle_double(matrix[count_x]);
-				new_line = ft_handledollar(data, new_line);
-				free (matrix[count_x]);
-				matrix[count_x] = new_line;
-				quote_found = 1;
-				break ;
-			}
+			quote_found = handle_quotes(data, matrix, count_x, count_y);
 			count_y++;
 		}
 		if (!quote_found)
@@ -121,5 +128,4 @@ void	ft_handle_quotes(t_data	*data, char	**matrix)
 		}
 		count_x++;
 	}
-	//printf("%s\n", new_line);
 }
