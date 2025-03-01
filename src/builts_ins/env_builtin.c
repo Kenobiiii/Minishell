a59077a282559ef6fe94dbab132c5739ea3cc27b
paromero <pablo.romeroponsfuster@gmail.com>
2025-03-01 16:55:21 +0100

    ahora si env tiene args da error

--- src/builts_ins/env_builtin.c
+++ src/builts_ins/env_builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env_builtin.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anggalle <anggalle@student.42.fr>          +#+  +:+       +#+        */
+/*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 18:03:42 by anggalle          #+#    #+#             */
-/*   Updated: 2025/01/29 18:10:40 by anggalle         ###   ########.fr       */
+/*   Updated: 2025/03/01 16:54:48 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ int	print_env(t_env *env)
 
 int	env_builtin(t_data *data)
 {
-	if (!data->ast->args[1])
-	{
+	if (!data->ast->args[2])
+		printf("Too many arguments\n");
+	else
 		print_env(data->env);
-	}
 	return (1);
 }
