f8ec61e76da4d064fd5e792a1617111c70cfa566
Angel <angelgallego999@gmail.com>
2025-03-29 00:14:58 +0100

    Permitir argumentos PWD

--- src/builts_ins/pwd_builtin.c
+++ src/builts_ins/pwd_builtin.c
@@ -6,7 +6,7 @@
 /*   By: anggalle <anggalle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 14:08:21 by anggalle          #+#    #+#             */
-/*   Updated: 2025/02/04 11:26:57 by anggalle         ###   ########.fr       */
+/*   Updated: 2025/03/29 00:14:37 by anggalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,6 @@ int	pwd_builtin(t_data *data)
 {
 	char	*path;
 
-	if (data->ast->args[1])
-	{
-		printf("Syntax error\n");
-		return (1);
-	}
 	path = find_variable_in_env(data, "PWD=");
 	ft_putstr_fd(path, STDOUT_FILENO);
 	ft_putstr_fd("\n", STDOUT_FILENO);
