4a57a35e8383095716b71344b3fefc440dca2874
paromero <pablo.romeroponsfuster@gmail.com>
2025-04-02 17:50:57 +0200

    arreglado pequeño leak

--- src/builts_ins/builtin.c
+++ src/builts_ins/builtin.c
@@ -6,7 +6,7 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 19:17:47 by anggalle          #+#    #+#             */
-/*   Updated: 2025/04/02 11:40:30 by paromero         ###   ########.fr       */
+/*   Updated: 2025/04/02 17:32:26 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 int	is_builtins(t_data *data, char *line)
 {
-	int	ret;
+	int		ret;
+	char	*wstatus;
 
+	wstatus = ft_itoa(data->wstatus);
 	ret = 0;
 	if (ft_strncmp(line, "echo", 5) == 0)
 		ret = echo_builtin(data);
 	else if (ft_strncmp(line, "pwd", 4) == 0)
 		ret = pwd_builtin(data);
-	else if (ft_strncmp(line, ft_itoa(data->wstatus), ft_strlen(line)) == 0)
+	else if (ft_strncmp(line, wstatus, ft_strlen(line)) == 0)
 		ret = last_cmd_status(data);
 	else if (ft_strncmp(line, "cd", 3) == 0)
 		ret = cd_builtin(data);
@@ -35,5 +37,6 @@ int	is_builtins(t_data *data, char *line)
 		ret = exit_builtin(data);
 	else if (ft_strncmp(line, "$EMPTY", 6) == 0)
 		ret = printf("\n");
+	free (wstatus);
 	return (ret);
 }
