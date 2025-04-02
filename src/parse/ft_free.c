48a26edd208e3a94dd602081eb6650e75350045a
paromero <pablo.romeroponsfuster@gmail.com>
2025-04-02 19:41:05 +0200

    optimizada liberación de memoria en ft_syntax

--- src/parse/ft_free.c
+++ src/parse/ft_free.c
@@ -6,13 +6,13 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 18:02:07 by paromero          #+#    #+#             */
-/*   Updated: 2025/02/11 18:31:12 by paromero         ###   ########.fr       */
+/*   Updated: 2025/04/02 19:24:23 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	free_innerwhile(t_data	*data)
+int	free_while(t_data	*data)
 {
 	if (data->line)
 	{
@@ -34,11 +34,12 @@ void	free_innerwhile(t_data	*data)
 		free (data->prompt);
 		data->prompt = NULL;
 	}
+	return (0);
 }
 
 void	free_minishell(t_data	*data)
 {
-	free_innerwhile(data);
+	free_while(data);
 	if (data->cwd)
 		free (data->cwd);
 	if (data->env)
