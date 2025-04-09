5b2f89670cecbd95a090c757aa08bca0f07e59c5
paromero <pablo.romeroponsfuster@gmail.com>
2025-04-09 18:22:25 +0200

    arreglada norminette de echo

--- src/builts_ins/echo_builtin.c
+++ src/builts_ins/echo_builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo_builtin.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anggalle <anggalle@student.42.fr>          +#+  +:+       +#+        */
+/*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 17:09:01 by anggalle          #+#    #+#             */
-/*   Updated: 2025/03/26 17:50:02 by anggalle         ###   ########.fr       */
+/*   Updated: 2025/04/09 18:01:02 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,77 +27,83 @@ static void	ft_putstatus_fd(char *status, int fd)
 
 char	*remove_mask(char *str)
 {
-    char	*cleaned;
-    int		i;
-    int		j;
+	char	*cleaned;
+	int		i;
+	int		j;
 
-    cleaned = malloc(ft_strlen(str) + 1);
-    if (!cleaned)
-        return (NULL);
-    i = 0;
-    j = 0;
-    while (str[i])
-    {
-        if (str[i] != '\x01') // Ignorar el marcador
-            cleaned[j++] = str[i];
-        i++;
-    }
-    cleaned[j] = '\0';
-    return (cleaned);
+	cleaned = malloc(ft_strlen(str) + 1);
+	if (!cleaned)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] != '\x01')
+			cleaned[j++] = str[i];
+		i++;
+	}
+	cleaned[j] = '\0';
+	return (cleaned);
+}
+
+static int	check_n_option(char *arg)
+{
+	int	j;
+
+	if (ft_strncmp(arg, "-n", 2) != 0)
+		return (0);
+	if (ft_strncmp(arg, "-n", ft_strlen(arg)) == 0)
+		return (1);
+	j = 2;
+	while (arg[j] == 'n')
+		j++;
+	if (arg[j])
+		return (0);
+	return (1);
+}
+
+static void	print_arg(t_data *data, char *arg, int has_next)
+{
+	char	*cleaned_arg;
+
+	if (ft_strncmp(arg, "$?", 2) == 0)
+		ft_putstatus_fd(ft_itoa(data->wstatus), STDOUT_FILENO);
+	else
+	{
+		cleaned_arg = remove_mask(arg);
+		if (cleaned_arg && cleaned_arg[0] != '\0')
+		{
+			ft_putstr_fd(cleaned_arg, STDOUT_FILENO);
+			if (has_next)
+				ft_putstr_fd(" ", STDOUT_FILENO);
+		}
+		free(cleaned_arg);
+	}
 }
 
 int	echo_builtin(t_data *data)
 {
 	int	i;
-	int	j;
 	int	print_new_line;
 
 	print_new_line = 1;
 	i = 1;
-
-	if (data->ast->args[1])
+	if (!data->ast->args[1])
 	{
-		while (data->ast->args[i])
-		{
-			if (ft_strncmp(data->ast->args[i], "-n", 2) == 0)
-			{
-				if (ft_strncmp(data->ast->args[i], "-n",
-						ft_strlen(data->ast->args[i])) == 0)
-					print_new_line = 0;
-				else
-				{
-					j = 2;
-					while (data->ast->args[i][j] == 'n')
-						j ++;
-					if (data->ast->args[i][j])
-					{
-						ft_putstr_fd(data->ast->args[i], STDOUT_FILENO);
-						if (data->ast->args[i + 1])
-							ft_putstr_fd(" ", STDOUT_FILENO);
-						print_new_line = 1;
-					}
-					else
-						print_new_line = 0;
-				}
-			}
-			else if (ft_strncmp(data->ast->args[i], "$?", 2) == 0)
-				ft_putstatus_fd(ft_itoa(data->wstatus), STDOUT_FILENO);
-			else
-			{
-				// Eliminar el marcador '\x01' antes de imprimir
-				char *cleaned_arg = remove_mask(data->ast->args[i]);
-				if (cleaned_arg && cleaned_arg[0] != '\0') // Verifica que no esté vacío
-				{
-					ft_putstr_fd(cleaned_arg, STDOUT_FILENO);
-					if (data->ast->args[i + 1])
-						ft_putstr_fd(" ", STDOUT_FILENO);
-				}
-				free(cleaned_arg);
-			}
-			i ++;
-		}
-		if (print_new_line)
-			ft_putstr_fd("\n", STDOUT_FILENO);
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		return (1);
 	}
+	while (data->ast->args[i] && check_n_option(data->ast->args[i]))
+	{
+		print_new_line = 0;
+		i++;
+	}
+	while (data->ast->args[i])
+	{
+		print_arg(data, data->ast->args[i], data->ast->args[i + 1] != NULL);
+		i++;
+	}
+	if (print_new_line)
+		ft_putstr_fd("\n", STDOUT_FILENO);
 	return (1);
 }
