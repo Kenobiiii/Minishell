a2c9ce8350b8ac23f51959ee0629b89dba59a1f9
paromero <pablo.romeroponsfuster@gmail.com>
2025-05-25 17:33:37 +0200

    norm redin2

--- src/parse/ft_redin2_cases.c
+++ src/parse/ft_redin2_cases.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_redin2_cases.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anggalle <anggalle@student.42.fr>          +#+  +:+       +#+        */
+/*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 13:19:35 by paromero          #+#    #+#             */
-/*   Updated: 2025/02/06 13:56:36 by anggalle         ###   ########.fr       */
+/*   Updated: 2025/05/25 17:33:18 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,44 +23,40 @@ int	is_redin2(t_ast **last_operator)
 	return (0);
 }
 
-//? Maneja el caso especial de heredoc (<<) con diferentes configuraciones de nodos
-void	redin2(t_ast **current_cmd, t_ast **last_operator,
-		t_ast *new_node, t_tokens *tokens)
+//? Maneja argumentos adicionales cuando ya hay comando y delimitador
+static void	handle_additional_args(t_ast **current_cmd, t_ast *new_node,
+		t_tokens *tokens)
 {
-	// Verificar que los punteros no sean NULL
-	if (!last_operator || !*last_operator || !new_node || !tokens)
-		return ;
-	
-	// Si no hay left (comando), asignar el comando
-	if (!(*last_operator)->left)
+	if (current_cmd && *current_cmd)
 	{
-		(*last_operator)->left = new_node;
-	}
-	// Si no hay right (delimitador), asignar el delimitador
-	else if (!(*last_operator)->right)
-	{
-		(*last_operator)->right = new_node;
+		ft_add_argument(*current_cmd, tokens->value);
+		if (new_node->value)
+			free(new_node->value);
+		free(new_node);
 	}
 	else
 	{
-		// Si ya hay comando y delimitador, tratar como argumento adicional
-		if (current_cmd && *current_cmd)
+		new_node->args = malloc(sizeof(char *) * 2);
+		if (new_node->args)
 		{
-			ft_add_argument(*current_cmd, tokens->value);
-			if (new_node->value)
-				free(new_node->value);
-			free(new_node);
+			new_node->args[0] = ft_strdup(tokens->value);
+			new_node->args[1] = NULL;
 		}
-		else
-		{
-			new_node->args = malloc(sizeof(char *) * 2);
-			if (new_node->args)
-			{
-				new_node->args[0] = ft_strdup(tokens->value);
-				new_node->args[1] = NULL;
-			}
-			if (current_cmd)
-				*current_cmd = new_node;
-		}
+		if (current_cmd)
+			*current_cmd = new_node;
 	}
+}
+
+//? Maneja el caso especial de heredoc (<<) con configuraciones de nodos
+void	redin2(t_ast **current_cmd, t_ast **last_operator,
+		t_ast *new_node, t_tokens *tokens)
+{
+	if (!last_operator || !*last_operator || !new_node || !tokens)
+		return ;
+	if (!(*last_operator)->left)
+		(*last_operator)->left = new_node;
+	else if (!(*last_operator)->right)
+		(*last_operator)->right = new_node;
+	else
+		handle_additional_args(current_cmd, new_node, tokens);
 }
