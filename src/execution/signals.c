ae79c6fe3aeb8e982228176c5b824e1fa698f60c
paromero <pablo.romeroponsfuster@gmail.com>
2025-05-25 21:49:39 +0200

    norminette de signals

--- src/execution/signals.c
+++ src/execution/signals.c
@@ -6,27 +6,28 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 18:42:47 by anggalle          #+#    #+#             */
-/*   Updated: 2025/04/09 17:40:15 by paromero         ###   ########.fr       */
+/*   Updated: 2025/05/25 21:49:03 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-extern volatile sig_atomic_t g_shell_state;
+extern volatile sig_atomic_t	g_shell_state;
 
 void	handle_sigint(int sig)
 {
 	(void)sig;
 	write(STDOUT_FILENO, "\n", 1);
-
-	if (g_shell_state == STATE_PROMPT_NORMAL || g_shell_state == STATE_PROMPT_INTERRUPTED)
+	if (g_shell_state == STATE_PROMPT_NORMAL
+		|| g_shell_state == STATE_PROMPT_INTERRUPTED)
 	{
 		g_shell_state = STATE_PROMPT_INTERRUPTED;
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
 	}
-	else if (g_shell_state == STATE_EXECUTING || g_shell_state == STATE_EXECUTION_INTERRUPTED)
+	else if (g_shell_state == STATE_EXECUTING
+		|| g_shell_state == STATE_EXECUTION_INTERRUPTED)
 	{
 		g_shell_state = STATE_EXECUTION_INTERRUPTED;
 	}
@@ -35,7 +36,8 @@ void	handle_sigint(int sig)
 void	handle_sigquit(int sig)
 {
 	(void)sig;
-	if (g_shell_state == STATE_EXECUTING || g_shell_state == STATE_EXECUTION_INTERRUPTED)
+	if (g_shell_state == STATE_EXECUTING
+		|| g_shell_state == STATE_EXECUTION_INTERRUPTED)
 	{
 		write(STDOUT_FILENO, "Quit: 3\n", 8);
 	}
