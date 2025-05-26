ad4d631cac04b714a75e28a5dbbedbf53f956dc3
paromero <pablo.romeroponsfuster@gmail.com>
2025-05-26 10:20:35 +0200

    arreglado segfault de ;

--- src/main_checks.c
+++ src/main_checks.c
@@ -6,7 +6,7 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 00:00:00 by anggalle          #+#    #+#             */
-/*   Updated: 2025/05/25 21:58:29 by paromero         ###   ########.fr       */
+/*   Updated: 2025/05/26 10:20:00 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,6 @@ int	check_line_errors(t_data *data)
 	if (!check_syntax_errors(data))
 		return (0);
 	if (!handle_invslash_pcomma(data->line))
-		return (free_while(0));
+		return (free_while(data));
 	return (1);
 }
